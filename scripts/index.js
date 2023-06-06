70995e2134f4913b82f65f500cf9f75a680776be
Barros Lucas <lucasbarros78@hotmail.com>
2023-06-06 20:02:13 -0300

    refinados estilos y comienzo de js

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    let uno = document.querySelector('#r1');
+    uno.addEventListener('click', function () {
+        uno.classList.add ('rating-selected') ;
+    })
+})
